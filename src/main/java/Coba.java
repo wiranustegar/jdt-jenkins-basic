665971af22f4be7d8c91db963d34cde1023634eb
tesaja <wrns.tegar@gmail.com>
2023-06-17 10:27:59 +0700

    pipe

--- src/main/java/Coba.java
+++ src/main/java/Coba.java
@@ -2,5 +2,7 @@ public class Coba {
     public static void main(String[] args) {
         System.out.println("Cobain pipeline jenkins");
         System.out.println("coba");
+
+        System.out.println("coba update");
     }
 }
